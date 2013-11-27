416562dba0fc9d93415b253893ceae7ee1ebf16e
Ruben Anton <ramtnez@gmail.com>
2013-11-27 22:09:00 +0100

    Fixed bug in config

--- config/env/all.js
+++ config/env/all.js
@@ -4,5 +4,6 @@ rootPath = path.normalize(__dirname + '/../..');
 module.exports = {
 	root: rootPath,
 	port: process.env.PORT || 3000,
+    // mongodb in localhost from production.json to avoid overwrite.
     db: process.env.MONGOHQ_URL || 'mongodb://localhost/mean'
 }
